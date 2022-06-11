02a44227e25d5d088b51045b836f3715c32008a9
sasaplus1 <sasaplus1@gmail.com>
2022-06-11 09:50:28 +0900

    take screenshot

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,4 +4,5 @@ test('basic test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   const title = page.locator('.navbar__inner .navbar__title');
   await expect(title).toHaveText('Playwright');
+  await page.screenshot({ path: `example.png` });
 });
